5e7cd708f6a23ad040bc89c65a20272e735fbafa
Marius1388 <mariuscristian.bancila@gmail.com>
2019-10-21 09:04:29 +0300

    updating database host

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-animated-50501',
       user : 'postgres',
       password : process.env.POSTRGRESPASSWORD,
       database : 'smart-brain'
