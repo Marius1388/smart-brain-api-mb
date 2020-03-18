ead855bfff3b2e3b1924d1ab45035eda8dd2ac13
Marius1388 <mariuscristian.bancila@gmail.com>
2020-03-18 14:22:15 +0200

    docker setup postgres local dev implemented

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt= require('bcrypt-nodejs');
 const cors =require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin =require('./controllers/signin');
@@ -12,22 +13,26 @@ const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
-    connection: {
-      // !!!!! commented lines are for deployment use only!!!!
-      // connectionString: process.env.DATABASE_URL,
-      // host : 'postgresql-animated-50501',
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : process.env.POSTRGRESPASSWORD,
-      database : 'smart-brain'
-      // ssl: true
-    }
+    // connection: {
+    //   // !!!!! commented lines are for deployment use only!!!!
+    //   // connectionString: process.env.DATABASE_URL,
+    //   // host : 'postgresql-animated-50501',
+    //   host : process.env.POSTGRES_HOST,
+    //   user : process.env.POSTGRES_USER,
+    //   password : process.env.POSTGRES_PASSWORD,
+    //   database : process.env.POSTGRES_DB
+    //   // ssl: true
+    // }
+    
+    connection: process.env.POSTGRES_URI
   });
 
 
 const app = express();
+// console.log('when?');
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('combined'));
 
 app.get('/',(req,res)=>{
     res.send('it is working');
@@ -48,6 +53,6 @@ app.put('/image', (req,res) => {image.handleImage(req,res,db)})
 app.post('/imageUrl', (req,res) => {image.handleApiCall(req,res)})
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`server is running on port ${process.env.PORT}`);
+app.listen( 3000 || process.env.PORT, ()=> {
+    console.log(`server is running on port 3000 or ${process.env.PORT}`);
 })
